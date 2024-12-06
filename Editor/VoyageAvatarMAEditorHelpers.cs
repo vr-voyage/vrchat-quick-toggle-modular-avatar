fc5b8da96fc7eda39a77084a3b71f033785ab1fc
Voyage <voyage@miouyouyou.fr>
2024-12-06 23:16:53 +0100

    Ready for shipping
    * Added the creation of a folder to store all the
      generated assets
    * Added the creation of a subfolder per avatar or
      standalone item
    * Added the ability to add toggles on multiple avatars
      and standalone items at the same time
    * Added Undo abilities
    * Reorganized how menu items are managed, in order to
      keep maximum flexibility.
    * Children added to manage the Menu Items of the
      GameObject selected for toggling are now tagged
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

--- Editor/VoyageAvatarMAEditorHelpers.cs
+++ Editor/VoyageAvatarMAEditorHelpers.cs
@@ -8,6 +8,7 @@ using System;
 using System.IO;
 using VRC.SDK3.Avatars.Components;
 using UnityEditor.Animations;
+using UnityEngine.UIElements;
 
 public class VoyageAvatarMAEditorHelpers
 {
@@ -59,22 +60,27 @@ public class VoyageAvatarMAEditorHelpers
     const string quickToggleTag = "QuickToggle-Tag";
     
 
-    static Transform PrepareMenuOnAvatar(GameObject firstItem)
+    static Transform PrepareMenuOnAvatar(GameObject item, out Transform avatarTransform)
     {
-        Transform avatar = FindAvatarRoot(firstItem.transform);
+
+        Transform avatar = FindAvatarRoot(item.transform);
+        avatarTransform = avatar;
         if (avatar == null)
         {
-            Debug.Log("No avatar !");
             return null;
         }
+        Undo.RecordObject(avatar, "[QT] Quick Toggle");
         Transform quickToggleTransform = avatar.Find(quickToggleMenuGoName);
 
         GameObject quickToggleObject;
         if (quickToggleTransform == null)
         {
             quickToggleObject = new GameObject(quickToggleMenuGoName);
+            Undo.RegisterCreatedObjectUndo(quickToggleObject, $"[QT] Create menu for {avatar.name}");
+            Undo.RecordObject(quickToggleObject.transform, $"[QT] Set {item.name} onto the avatar");
             quickToggleTransform = quickToggleObject.transform;
             quickToggleTransform.parent = avatar;
+            
         }
 
         Transform togglesMenuTransform = null;
@@ -89,25 +95,28 @@ public class VoyageAvatarMAEditorHelpers
         if (togglesMenuTransform == null)
         {
             GameObject go = new GameObject("Toggles");
+            Undo.RegisterCreatedObjectUndo(go, $"[QT] Create a GameObject for {avatar.name} Toggles");
+            Undo.RecordObject(go.transform, $"[QT] Setting Toggles as a child of the Quick Toggles menu object");
             togglesMenuTransform = go.transform;
             togglesMenuTransform.parent = quickToggleTransform;
         }
 
-        ModularAvatarMenuItem modularAvatarMenu = togglesMenuTransform.GetComponent<ModularAvatarMenuItem>();
+        GameObject togglesMenuObject = togglesMenuTransform.gameObject;
+
+        ModularAvatarMenuItem modularAvatarMenu = togglesMenuObject.GetComponent<ModularAvatarMenuItem>();
         if (modularAvatarMenu == null)
         {
-            modularAvatarMenu = togglesMenuTransform.gameObject.AddComponent<ModularAvatarMenuItem>();
+
+            modularAvatarMenu = Undo.AddComponent<ModularAvatarMenuItem>(togglesMenuObject);
         }
         modularAvatarMenu.Control.type = VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control.ControlType.SubMenu;
         modularAvatarMenu.MenuSource = SubmenuSource.Children;
 
-        ModularAvatarMenuInstaller modularAvatarMenuInstaller = togglesMenuTransform.GetComponent<ModularAvatarMenuInstaller>();
+        ModularAvatarMenuInstaller modularAvatarMenuInstaller = togglesMenuObject.GetComponent<ModularAvatarMenuInstaller>();
         if (modularAvatarMenuInstaller == null)
         {
-            modularAvatarMenuInstaller = togglesMenuTransform.gameObject.AddComponent<ModularAvatarMenuInstaller>();
+            modularAvatarMenuInstaller = Undo.AddComponent< ModularAvatarMenuInstaller>(togglesMenuObject);
         }
-
-        
 
         return togglesMenuTransform;
 
@@ -120,6 +129,7 @@ public class VoyageAvatarMAEditorHelpers
 
         GameObject menuInstallerTarget = AssetDatabase.LoadAssetAtPath<GameObject>(menuInstallTargetPrefabPath);
         GameObject prefabInstance = GameObject.Instantiate(menuInstallerTarget, modularAvatarMenuRoot);
+        Undo.RegisterCreatedObjectUndo(prefabInstance, $"[QT] Quick Toggle");
         prefabInstance.name = itemName;
 
         Component[] components = prefabInstance.GetComponents<Component>();
@@ -170,17 +180,142 @@ public class VoyageAvatarMAEditorHelpers
         return transition;
     }
 
-        [MenuItem("GameObject/Voyage/Invididual toggle (Modular Avatar)")]
+    public static AnimatorController CreateToggleItemController(
+        GameObject selectedGameObject,
+        string projectRelativePath,
+        string itemName,
+        string parameterName,
+        string suffix,
+        string subFolderName)
+    {
+
+        string fsFriendlySubFolderName = FilesystemFriendlyName(subFolderName.Replace(" ", "_"));
+
+        string generatedAssetsPath = projectRelativePath + '/' + fsFriendlySubFolderName;
+        if (!AssetDatabase.IsValidFolder(generatedAssetsPath))
+        {
+            string createdFolderGuid = AssetDatabase.CreateFolder(projectRelativePath, fsFriendlySubFolderName);
+            if (createdFolderGuid == "")
+            {
+                Debug.LogError($"Could not create subdirectory {generatedAssetsPath}");
+                return null;
+            }
+            generatedAssetsPath = AssetDatabase.GUIDToAssetPath(createdFolderGuid);
+        }
+
+
+
+        string itemFsFriendlyName = FilesystemFriendlyName(itemName.Replace(" ", "_"));
+        
+        string stateMachineName = $"Toggle_{itemFsFriendlyName}";
+        
+        AnimationClip offClip = new AnimationClip { name = $"{stateMachineName}-off", wrapMode = WrapMode.Once };
+        AnimationClip onClip = new AnimationClip { name = $"{stateMachineName}-on", wrapMode = WrapMode.Once };
+
+        offClip.SetCurve(itemName, typeof(GameObject), "m_IsActive", AnimationCurve.Constant(0, 1 / 60f, 0));
+        onClip.SetCurve(itemName, typeof(GameObject), "m_IsActive", AnimationCurve.Constant(0, 1 / 60f, 1));
+
+        AssetDatabase.CreateAsset(offClip, $"{generatedAssetsPath}/{offClip.name}-{suffix}.anim");
+        AssetDatabase.CreateAsset(onClip, $"{generatedAssetsPath}/{onClip.name}-{suffix}.anim");
+
+        AnimatorController generatedFxController = AnimatorController.CreateAnimatorControllerAtPath($"{generatedAssetsPath}/FX_Toggle_Item_{itemFsFriendlyName}-{suffix}.controller");
+        generatedFxController.AddParameter(
+            new AnimatorControllerParameter
+            {
+                name = parameterName,
+                type = AnimatorControllerParameterType.Bool,
+                defaultBool = selectedGameObject.activeSelf
+            }
+        );
+
+        AnimatorStateMachine stateMachine = new AnimatorStateMachine();
+        stateMachine.name = $"Toggle_{itemFsFriendlyName}";
+
+        var hideState = stateMachine.AddState("OFF");
+        hideState.motion = offClip;
+        hideState.writeDefaultValues = false;
+
+        var showState = stateMachine.AddState("ON");
+        showState.motion = onClip;
+        showState.writeDefaultValues = false;
+
+        if (selectedGameObject.activeSelf)
+        {
+            stateMachine.defaultState = showState;
+            var toHideTransition = showState.AddTransition(hideState);
+            MakeInstant(toHideTransition).AddCondition(AnimatorConditionMode.IfNot, 1, parameterName);
+
+            var toExitTransition = hideState.AddExitTransition();
+            MakeInstant(toExitTransition).AddCondition(AnimatorConditionMode.If, 1, parameterName);
+        }
+        else
+        {
+            stateMachine.defaultState = hideState;
+            var toShowTransition = hideState.AddTransition(showState);
+            MakeInstant(toShowTransition).AddCondition(AnimatorConditionMode.If, 1, parameterName);
+
+            var toExitTransition = showState.AddExitTransition();
+            MakeInstant(toExitTransition).AddCondition(AnimatorConditionMode.IfNot, 1, parameterName);
+        }
+
+        ControllerAddLayer(generatedFxController, stateMachine);
+
+        EditorUtility.SetDirty(stateMachine);
+        EditorUtility.SetDirty(generatedFxController);
+
+        AssetDatabase.Refresh();
+
+        return generatedFxController;
+    }
+
+    [MenuItem("GameObject/Voyage/Is Valid Object")]
+    public static void IsValidObject(MenuCommand menuCommand)
+    {
+        GameObject go = Selection.activeGameObject;
+        if (go == null || go.scene == null || go.scene.IsValid() == false)
+        { 
+            Debug.Log("Invalid");
+            return;
+        }
+    }
+
+    public static bool ShouldIgnoreGameObject(GameObject go)
+    {
+        return
+            go == null
+            || go.TryGetComponent<ModularAvatarMergeAnimator>(out var _) == true
+            || go.TryGetComponent<ModularAvatarParameters>(out var _) == true;
+    }
+
+    public static void AddTagIfNotPresent(string tagToAdd)
+    {
+        string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+        foreach (string tag in tags)
+        {
+            if (tag == tagToAdd) { return; }
+        }
+
+        UnityEditorInternal.InternalEditorUtility.AddTag(tagToAdd);
+    }
+
+    [MenuItem("GameObject/Voyage/Invididual toggle (Modular Avatar)")]
     public static void ToggleThroughMenuMa(MenuCommand menuCommand)
     {
+        
+
         if (menuCommand.context != Selection.activeGameObject) { return; }
+
+        var currentUndoGroup = Undo.GetCurrentGroup();
 
         GameObject[] gameObjects = Selection.gameObjects;
         if (gameObjects.Length == 0) { return; }
 
         TryGetActiveFolderPath(out string outFolderPath);
-        var assetsFolder = Application.dataPath;
 
+        /* Let's avoid leading the user to the 'Packages' directory, this might lead to disaster */
+        outFolderPath = (outFolderPath.StartsWith("Assets") ? outFolderPath : "Assets");
+
+        var assetsFolder = Application.dataPath;
 
         string saveDirPath = EditorUtility.SaveFolderPanel("Where to save the animations", outFolderPath, "");
         if (saveDirPath.Length == 0) { return; }
@@ -189,89 +324,72 @@ public class VoyageAvatarMAEditorHelpers
             return;
         }
         string assetsRelativePath = saveDirPath.Substring(assetsFolder.Length);
-        if (assetsRelativePath.StartsWith('/')) assetsRelativePath += "/";
+        //if (assetsRelativePath.StartsWith('/')) assetsRelativePath += "/";
 
+        string suffix = DateTime.Now.ToString("s").Replace(":", "");
         string projectRelativePath = $"Assets/{assetsRelativePath}";
+        string folderName = $"QuickToggle-{suffix}";
 
-        Transform menuTransform = PrepareMenuOnAvatar(gameObjects[0]);
-
-        if (menuTransform == null)
+        if (!AssetDatabase.IsValidFolder(projectRelativePath))
         {
+            Debug.LogError("Could not determine the save relative path");
             return;
         }
 
+        string generatedFolderGuid = AssetDatabase.CreateFolder(projectRelativePath, folderName);
+
+        if (generatedFolderGuid == "")
+        {
+            Debug.LogError($"generatedFolderGuid : {generatedFolderGuid} - Asked for {projectRelativePath}, {folderName}");
+            Debug.LogError("Could not create a folder to store the assets to generate");
+            return;
+        }
+
+        string generatedAssetsRelativePath = AssetDatabase.GUIDToAssetPath(generatedFolderGuid);
+
+        Transform lastMenuAdded = null;
+        AddTagIfNotPresent(quickToggleTag);
         foreach (var selectedGameObject in gameObjects)
         {
-            if (selectedGameObject == null) return;
+            if (ShouldIgnoreGameObject(selectedGameObject))
+            {
+                Debug.LogWarning($"[Quick Toggle] Ignoring {selectedGameObject}");
+                continue;
+            }
+            Transform menuTransform = PrepareMenuOnAvatar(selectedGameObject, out Transform avatarTransform);
+
+            Undo.RecordObject(selectedGameObject.transform, $"[QT] Recording the transform of {selectedGameObject.name}");
 
             string itemName = selectedGameObject.name;
             string itemFsFriendlyName = FilesystemFriendlyName(itemName.Replace(" ", "_"));
             string parameterName = $"toggle_{itemFsFriendlyName}";
-            string stateMachineName = $"Toggle_{itemFsFriendlyName}";
-            string currentTimeWithoutColons = DateTime.Now.ToString("s").Replace(":", "");
 
+            if (selectedGameObject == null) return;
 
-
-            AnimationClip offClip = new AnimationClip { name = $"{stateMachineName}-off", wrapMode = WrapMode.Once };
-            AnimationClip onClip = new AnimationClip { name = $"{stateMachineName}-on", wrapMode = WrapMode.Once };
-
-            offClip.SetCurve("", typeof(GameObject), "m_IsActive", AnimationCurve.Constant(0, 1 / 60f, 0));
-            onClip.SetCurve("", typeof(GameObject), "m_IsActive", AnimationCurve.Constant(0, 1 / 60f, 1));
-
-            AssetDatabase.CreateAsset(offClip, $"{projectRelativePath}/{offClip.name}-{currentTimeWithoutColons}.anim");
-            AssetDatabase.CreateAsset(onClip, $"{projectRelativePath}/{onClip.name}-{currentTimeWithoutColons}.anim");
-
-            AnimatorController generatedFxController = AnimatorController.CreateAnimatorControllerAtPath($"{projectRelativePath}/FX_Toggle_Item_{itemFsFriendlyName}-{currentTimeWithoutColons}.controller");
-            generatedFxController.AddParameter(
-                new AnimatorControllerParameter
-                {
-                    name = parameterName,
-                    type = AnimatorControllerParameterType.Bool,
-                    defaultBool = selectedGameObject.activeSelf
-                }
-            );
-
-            AnimatorStateMachine stateMachine = new AnimatorStateMachine();
-            stateMachine.name = $"Toggle_{itemFsFriendlyName}";
-
-            var hideState = stateMachine.AddState("OFF");
-            hideState.motion = offClip;
-            hideState.writeDefaultValues = false;
-
-            var showState = stateMachine.AddState("ON");
-            showState.motion = onClip;
-            showState.writeDefaultValues = false;
-
-            if (selectedGameObject.activeSelf)
+            AnimatorController generatedFxController = CreateToggleItemController(
+                selectedGameObject, generatedAssetsRelativePath,
+                itemName, parameterName,
+                selectedGameObject.GetInstanceID().ToString(),
+                avatarTransform != null ? avatarTransform.name : itemName);
+            if (generatedFxController == null)
             {
-                stateMachine.defaultState = showState;
-                var toHideTransition = showState.AddTransition(hideState);
-                MakeInstant(toHideTransition).AddCondition(AnimatorConditionMode.IfNot, 1, parameterName);
-
-                var toExitTransition = hideState.AddExitTransition();
-                MakeInstant(toExitTransition).AddCondition(AnimatorConditionMode.If, 1, parameterName);
-            }
-            else
-            {
-                stateMachine.defaultState = hideState;
-                var toShowTransition = hideState.AddTransition(showState);
-                MakeInstant(toShowTransition).AddCondition(AnimatorConditionMode.If, 1, parameterName);
-
-                var toExitTransition = showState.AddExitTransition();
-                MakeInstant(toExitTransition).AddCondition(AnimatorConditionMode.IfNot, 1, parameterName);
+                Debug.LogWarning($"Could not create an animator for {selectedGameObject.name}. Skipping");
+                continue;
             }
 
-            ControllerAddLayer(generatedFxController, stateMachine);
+            bool createAnimator = (selectedGameObject.TryGetComponent<Animator>(out Animator _) == false);
+            
+            if (createAnimator)
+            {
+                Animator animator = Undo.AddComponent<Animator>(selectedGameObject);
+                animator.runtimeAnimatorController = generatedFxController;
+            }
 
-            EditorUtility.SetDirty(stateMachine);
-            EditorUtility.SetDirty(generatedFxController);
-
-            AssetDatabase.Refresh();
-
-            var mergeAnimator = selectedGameObject.AddComponent<ModularAvatarMergeAnimator>();
+            var mergeAnimator = Undo.AddComponent<ModularAvatarMergeAnimator>(selectedGameObject);
             mergeAnimator.animator = generatedFxController;
+            mergeAnimator.deleteAttachedAnimator = createAnimator;
 
-            var avatarParameters = selectedGameObject.AddComponent<ModularAvatarParameters>();
+            var avatarParameters = Undo.AddComponent<ModularAvatarParameters>(selectedGameObject);
             float defaultValue = selectedGameObject.activeSelf ? 1f : 0f;
 
             avatarParameters.parameters.Add(
@@ -284,11 +402,12 @@ public class VoyageAvatarMAEditorHelpers
                     internalParameter = true
                 });
 
-            var menuItemObject = new GameObject();
+            var menuItemObject = new GameObject() { name = selectedGameObject.name, tag = quickToggleTag };
+            Undo.RegisterCreatedObjectUndo(menuItemObject, "[QT] Add a child to represent the Menu Item");
+            Undo.RecordObject(menuItemObject.transform, "[QT] Setting the object transform");
             menuItemObject.transform.parent = selectedGameObject.transform;
-            menuItemObject.name = selectedGameObject.name;
 
-            var menuItem = menuItemObject.AddComponent<ModularAvatarMenuItem>();
+            var menuItem = Undo.AddComponent<ModularAvatarMenuItem>(menuItemObject);
             menuItem.Control.type = VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control.ControlType.Toggle;
             menuItem.Control.name = itemName;
             menuItem.Control.parameter = new VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu.Control.Parameter()
@@ -296,18 +415,27 @@ public class VoyageAvatarMAEditorHelpers
                 name = parameterName
             };
 
-            var menuItemInstaller = menuItemObject.AddComponent<ModularAvatarMenuInstaller>();
+            var menuItemInstaller = Undo.AddComponent<ModularAvatarMenuInstaller>(menuItemObject);
 
-            AddItemToModularAvatarMenu(menuTransform, selectedGameObject.name, menuItemInstaller);
-            
+            if (menuTransform != null)
+            {
+                AddItemToModularAvatarMenu(menuTransform, selectedGameObject.name, menuItemInstaller);
+                lastMenuAdded = menuTransform;
+            }
+
         }
 
-        Selection.activeGameObject = menuTransform.gameObject;
+        if (lastMenuAdded != null)
+        {
+            Selection.activeGameObject = lastMenuAdded.gameObject;
+        }
+
+        Undo.CollapseUndoOperations(currentUndoGroup);
 
 
-        
 
     }
- 
+
+
 }
 #endif
